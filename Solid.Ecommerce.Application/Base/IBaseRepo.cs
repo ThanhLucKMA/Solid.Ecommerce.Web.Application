5ab86dbe385d0b201cb4acafe895e4eca91903a2
Thanh Luc <Thanh Luc@ADMIN-PC>
2023-05-14 16:59:05 +0700

    Add project files.

---
+++ Solid.Ecommerce.Application/Base/IBaseRepo.cs
@@ -0,0 +1,7 @@
+﻿namespace Solid.Ecommerce.Application.Base;
+public interface IBaseRepo<T> where T : class
+{
+    DbSet<T> Entities { get; }
+    IApplicationDBContext ApplicationDBContext { get; } //Has
+}
+
---
+++ Solid.Ecommerce.UI.Test/Program.cs
@@ -0,0 +1,29 @@
+﻿
+using Solid.Ecommerce.Application;
+using Solid.Ecommerce.Application.Interfaces.Common;
+using Solid.Ecommerce.Application.Interfaces.Repositories;
+using Solid.Ecommerce.Infrastructure.Context;
+using Solid.Ecommerce.Infrastructure.Repositories;
+using Solid.Ecommerce.Shared;
+
+
+namespace Solid.Ecommerce.UI.Test;
+public class Program
+{
+    public  static void Main(string[] args)
+    {
+
+        DbFactoryContext dbContext = new DbFactoryContext(() => new SolidEcommerceDbContext());
+
+        IApplicationDBContext db = new ApplicationDbContext(dbContext);
+
+        IRepository<Product> r = new Repository<Product>(db);
+        var data =  r.Find(1);
+
+        Console.WriteLine($"Product ID: {data.ProductId}, Product Name: {data.Name}");
+        
+
+
+        Console.ReadLine();
+    }
+}
